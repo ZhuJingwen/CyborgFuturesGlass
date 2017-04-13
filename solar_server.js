7ccecc20f0a033d8b22cf75ff37c1c976a7e4331
Kyle James <kyle1james@gmail.com>
2017-04-13 16:02:20 -0400

    Update solar_server.js

--- solar_server.js
+++ solar_server.js
@@ -24,5 +24,5 @@ request(options, function(err, res, body) {
 //request('https://blue-bean.herokuapp.com/index', function (error, response, body) {
 //  console.log('error:', error); // Print the error if one occurred 
 //  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-//  console.log('body:', body); // Print the HTML for the Google homepage. 
+//  console.log('body:', body);  
 //});
